1ab0e27a42bed1d7fffedcdf0f973288b2d66be6
Matthew Harmon <matt.harmon@skillset.com>
2015-06-02 06:58:34 -0500

    Got the project functional.

--- app/src/main/java/com/project0/matthewharmon/project0/MainActivity.java
+++ app/src/main/java/com/project0/matthewharmon/project0/MainActivity.java
@@ -1,17 +1,42 @@
 package com.project0.matthewharmon.project0;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    protected Button btnSpotifyStreamer;
+    protected Button btnScoresApp;
+    protected Button btnLibraryApp;
+    protected Button btnBuildItBigger;
+    protected Button btnXyzReader;
+    protected Button btnCapstone;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Build out the layout items.
+        this.btnSpotifyStreamer = (Button) findViewById(R.id.btn_spotify_streamer);
+        this.btnScoresApp = (Button) findViewById(R.id.btn_scores_app);
+        this.btnLibraryApp = (Button) findViewById(R.id.btn_library_app);
+        this.btnBuildItBigger = (Button) findViewById(R.id.btn_build_it_bigger);
+        this.btnXyzReader = (Button) findViewById(R.id.btn_xyz_reader);
+        this.btnCapstone = (Button) findViewById(R.id.btn_capstone);
+
+        // Add the on click listeners.
+        this.btnSpotifyStreamer.setOnClickListener(this);
+        this.btnScoresApp.setOnClickListener(this);
+        this.btnLibraryApp.setOnClickListener(this);
+        this.btnBuildItBigger.setOnClickListener(this);
+        this.btnXyzReader.setOnClickListener(this);
+        this.btnCapstone.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +59,24 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Display a toast for the buttons upon click.
+     *
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+        String btnText = button.getText().toString();
+        String message = String.format(
+            "This button will launch the %s app!",
+            btnText
+        );
+        Context appContext = view.getContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(appContext, message, duration);
+        toast.show();
     }
 }
